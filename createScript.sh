a0416df9891c0f754b40ece984e15a699ca2f36a
Prince Bansal <prince.bansal008@gmail.com>
2023-03-06 18:58:10 +0530

    updated file

--- createScript.sh
+++ createScript.sh
@@ -2,8 +2,14 @@
 
 echo "Enter file name with extension:"
 read FILE
+
+# creating FILE Scripts directory with executable permission
+
 cd Scripts/
 touch $FILE
 chmod 755 $FILE
+
+# Appending basic code in the FILE
+echo "#! /bin/zsh" >> $FILE
 
 echo "File created successfully!"
