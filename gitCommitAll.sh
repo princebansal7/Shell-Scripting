a886e578fd4229e37363af3f47fc7692ca9c774b
Prince Bansal <prince.bansal008@gmail.com>
2023-03-07 22:35:58 +0530

    git script updated

--- gitCommitAll.sh
+++ gitCommitAll.sh
@@ -12,10 +12,10 @@ read -p "PRESS Y for YES else PRESS anything: " ANS
 if [ $ANS == "Y" -o $ANS == "y" ]; then
     git push
     echo ""
-    echo "All files committed & pushed successfully!"
+    echo "---[All files committed & pushed successfully!]---"
     echo ""
 else
     echo ""
     echo ""
-    echo "All files added & committed sucessfully"
+    echo "---[All files added & committed sucessfully]---"
 fi
