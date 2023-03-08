7be53b3c130107f6b0ffc100b913afed1b11811d
Prince Bansal <prince.bansal008@gmail.com>
2023-03-08 15:44:25 +0530

    updated

--- gitCommitAll.sh
+++ gitCommitAll.sh
@@ -6,7 +6,7 @@ git add .
 git commit -m "$MSG"
 
 echo ""
-echo "Do you want to push committed files? (Y/N)"
+echo "Do you want to push committed files? (Y/N): "
 read ANS
 
 if [ $ANS == "Y" -o $ANS == "y" ]; then
